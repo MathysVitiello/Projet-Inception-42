c830a378cbab6274f732d992d15aa97bf4b5c22d
MathysVitiello <mathys.vitiello@gmail.com>
2024-04-22 11:23:07 -0400

    bcp de changement, bientot done

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,21 +1,15 @@
-chmod 644 /etc/mysql/mariadb.conf.d/50-server.cnf
-echo "........000000000000"
-
+#!/bin/bash
+# chmod 644 /etc/mysql/mariadb.conf.d/50-server.cnf
 
 service mariadb start;
-echo "111111111111"
-# su - // jai essaye de strucs mais ke me comrped tj pas, oklm
-mysql -u root
-echo "23333333333333333"
 
-sleep 10;
+sleep 2;
 
-echo "bbbbbbBB"
 mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-echo "AAAAA"
 mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+# mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mariadb -e "FLUSH PRIVILEGES;"
 mariadb-admin -u root -p$SQL_ROOT_PASSWORD shutdown
 mysqld
+echo "mamaaa"
